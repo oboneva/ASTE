d66bc4afd74768bf9f9eb0327e58e9b0380dbd46
oboneva <ogi.boneva@gmail.com>
2021-08-27 11:27:15 +0300

    update configs

--- configs.py
+++ configs.py
@@ -1,11 +1,11 @@
 class data_configs:
     data_dir = "./data/penga/14lap"
-    train_batch_size = 12
-    test_batch_size = 12
-    val_batch_size = 12
+    train_batch_size = 128
+    test_batch_size = 64
+    val_batch_size = 64
     num_workers = 6
 
 
 class trainer_configs:
-    epochs = 100
+    epochs = 500
     patience = 15
